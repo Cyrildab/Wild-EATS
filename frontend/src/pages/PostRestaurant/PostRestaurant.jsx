b33b40972b9d45ebe5781fbe7d3a38a9c31cd96f
CYRIL DABURON <cyril.daburon@gmail.com>
2024-02-02 13:26:14 +0100

    remove div

--- frontend/src/pages/PostRestaurant/PostRestaurant.jsx
+++ frontend/src/pages/PostRestaurant/PostRestaurant.jsx
@@ -77,16 +77,14 @@ function PostRestaurant() {
         {...register("alt")}
       />
 
-      <div className="selectFilter">
-        <select {...register("categorie")} id="categorie" className="imputPost">
-          <option value="Burger">Burger</option>
-          <option value="Japonais">Japonais</option>
-          <option value="Chinois">Chinois</option>
-          <option value="Français">Français</option>
-          <option value="Mexicain">Mexicain</option>
-          <option value="Italien">Italien</option>
-        </select>
-      </div>
+      <select {...register("categorie")} id="categorie" className="imputPost">
+        <option value="Burger">Burger</option>
+        <option value="Japonais">Japonais</option>
+        <option value="Chinois">Chinois</option>
+        <option value="Français">Français</option>
+        <option value="Mexicain">Mexicain</option>
+        <option value="Italien">Italien</option>
+      </select>
 
       <input
         className="imputPost"
@@ -94,15 +92,12 @@ function PostRestaurant() {
         placeholder="Distance"
         {...register("distance")}
       />
-
-      <div className="selectFilter">
-        <select {...register("price")} id="price" className="imputPost">
-          <option value="€">€</option>
-          <option value="€€">€€</option>
-          <option value="€€€">€€€</option>
-          <option value="€€€€">€€€€</option>
-        </select>
-      </div>
+      <select {...register("price")} id="price" className="imputPost">
+        <option value="€">€</option>
+        <option value="€€">€€</option>
+        <option value="€€€">€€€</option>
+        <option value="€€€€">€€€€</option>
+      </select>
 
       <input
         placeholder="Adresse"
